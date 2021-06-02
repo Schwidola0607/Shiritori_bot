58fc018d07b1d221632a5c69ac1cf0b31fb988cf
Schwidola0607 <khoadangpham82944@gmail.com>
2021-06-02 21:05:13 +0700

    Updates

--- bot.py
+++ bot.py
@@ -1,33 +1,36 @@
+import discord
+from PyDictionary import PyDictionary
 from discord.ext import commands
-import random
-bot = commands.Bot(command_prefix = "!")
-@bot.command(name = "idea", help = "get a side project idea")
-async def idea(ctx, help = "get a side project idea"):
-    await ctx.send("Ideas are hard")
-    await ctx.send("Worry not")
-    topics = ['chat bot', 'cli', 'game', 'web bot', 'browser extension', 'api', 'web interface']
-    area = ['note taking', 'social life', 'physical fitness', 'mental health', 'pet care']
-    language = ['c++', 'python3', 'java', 'R']
-    idea = f'Create a new {random.choice(topics)} by {random.choice(language)} that helps with {random.choice(area) }! :sunglasses:'
-    await ctx.send(idea)
+import os
+from dotenv import load_dotenv
+load_dotenv()
+intents = discord.Intents.all()
+Dictionary = PyDictionary()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-@bot.command(name = "calc", help = "Perform a calculation where fn is either -, +, *, /, %, or **")
-async def calc(ctx, x: float, fn: str, y: float):
-    if fn == '-':
-        await ctx.send(x - y)
-    elif fn == '+':
-        await ctx.send(x + y)
-    elif fn == '*':
-        await ctx.send(x * y)
-    elif fn == '/':
-        await ctx.send(x / y)
-    elif fn == "**":
-        await ctx.send(x ** y)
-    elif fn == '%':
-        await ctx.send(x % y)
-    else:
-        await ctx.send("We are not currently supported that function, Please try again")
-with open("BOT_TOKEN.txt", "r") as token_file:
-    TOKEN = token_file.read()
-    print("Token file read") 
-    bot.run(TOKEN)
+bot = commands.Bot(command_prefix = "&", intents = intents)
+@bot.command(name = 'test')
+async def test(ctx):
+    guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
+    if(ctx.message.author == guild.owner):
+        await ctx.send("Baka!!")
+
+@bot.command(name = 'mean', help = "return the meaning of a string")
+async def get_mean(ctx, word: str, word_type):
+    temporary_dict = Dictionary.meaning(word)
+    for s in temporary_dict[word_type]:
+        await ctx.send(f'-{s}')
+
+@bot.event
+async def on_ready():        
+    guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
+    print(
+        f'{bot.user} has connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'   
+        )
+    members ='\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n -{members}')
+
+
+bot.run(TOKEN)
